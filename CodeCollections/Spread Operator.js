ddda1395a251d365c8e863a8c8d916858c3554a0
BrianJ-Code <55200248+BrianJ-Code@users.noreply.github.com>
2019-10-03 14:48:50 -0400

    S

---
+++ CodeCollections/Spread Operator.js
@@ -0,0 +1,13 @@
+<script>
+    const featured = ['Deep Dish','Peperoni','Hawaiian']
+    const specialty = ['Meatzza', 'Spicy Mama', 'Margherita']
+    consta pizzas = featured.concat(specialty);
+    console.log(pizzas);
+
+    const deepDish = {
+        pizzaName: 'Deep Dish'
+        size: 'Medium',
+        ingredients: ['Marinara', 'Italian Sausage', 'Dough', 'Cheese']
+    };
+    
+</script>
